a61bad32c2305d23b144a810c6ea7ab884344d14
Thiago <diasthiago@protonmail.com>
2025-01-30 12:23:53 -0300

    Update common.mk

--- common.mk
+++ common.mk
@@ -249,7 +249,6 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
 # Java Optimizations
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-SYSTEM_OPTIMIZE_JAVA := true
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
